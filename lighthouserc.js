20f3f140cd8e5cb395ae8d623667405a780aa13f
Victor Scaiceanu <victor.scaiceanu@amadeus.com>
2020-08-30 23:58:05 +0200

    feat: add lighthouserc.js

---
+++ lighthouserc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ci: {
+      upload: {
+        target: 'lhci',
+        serverBaseUrl: 'https://intense-escarpment-03506.herokuapp.com/',
+        token: 'bf001f62-af41-4afb-afd4-5a0c3b6b4303', // could also use LHCI_TOKEN variable instead
+      },
+    },
+  };
